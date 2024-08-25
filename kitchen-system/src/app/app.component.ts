70d7338c8e8f33569cbc422fb833f260f07f1245
ayaadel1346 <ayaadel1346@gmail.com>
2024-08-25 18:50:39 +0300

    fix login and register design

--- kitchen-system/src/app/app.component.ts
+++ kitchen-system/src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { Event, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'kitchen-system';
+
+export class AppComponent implements OnDestroy {
+  addClass: boolean = true;
+  private routerSubscription: Subscription;
+
+  constructor(private router: Router) {
+    this.routerSubscription = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: Event) => {
+        const navEndEvent = event as NavigationEnd;
+        this.addClass = !(navEndEvent.urlAfterRedirects.includes('/login') || navEndEvent.urlAfterRedirects.includes('/register'));
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
   
 }
